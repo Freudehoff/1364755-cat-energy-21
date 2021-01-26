3c1e1f71c113b53c91f17e64cd7983667a55a3e0
Вячеслав Жубрин <vyacheslavzhubrin@gmail.com>
2021-01-26 21:41:18 +0300

    финальная правка финал

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,14 @@
-  var navMain = document.querySelector('.main-navigation');
-  var navToggle = document.querySelector('.main-navigation__toggle');
+  var navMain = document.querySelector('.page-header__main-navigation');
+  var navToggle = document.querySelector('.page-header__main-navigation-toggle');
 
-  navMain.classList.remove('main-navigation--nojs');
+  navMain.classList.remove('page-header__main-navigation--nojs');
 
   navToggle.addEventListener('click', function() {
-    if (navMain.classList.contains('main-navigation--closed')) {
-      navMain.classList.remove('main-navigation--closed');
-      navMain.classList.add('main-navigation--opened');
+    if (navMain.classList.contains('page-header__main-navigation--closed')) {
+      navMain.classList.remove('page-header__main-navigation--closed');
+      navMain.classList.add('page-header__main-navigation--opened');
     } else {
-      navMain.classList.add('main-navigation--closed');
-      navMain.classList.remove('main-navigation--opened');
+      navMain.classList.add('page-header__main-navigation--closed');
+      navMain.classList.remove('page-header__main-navigation--opened');
     }
   });
